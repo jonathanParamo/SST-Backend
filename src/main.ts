3414e70f3f7172081350a5267dcee3f4f1389236
jonathanPramo <jonathan.guaydia@devnavigate.com>
2025-08-26 21:42:37 -0500

    chore: initial NestJS setup

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('dev'));
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `🚀 App running on http://localhost:${process.env.PORT ?? 3000}/api`,
+  );
+}
+bootstrap();
